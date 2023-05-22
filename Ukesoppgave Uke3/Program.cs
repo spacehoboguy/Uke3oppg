62f97486d55a0a5ad44ad90aa2552c03b8f85789
Philip Olafsen <ph.olafsen@gmail.com>
2023-05-22 12:07:08 +0200

    ukesoppgave opplastning to github

---
+++ Ukesoppgave Uke3/Program.cs
@@ -0,0 +1,10 @@
+﻿using Ukesoppgave_Uke3;
+
+var dragon = new VirtualPet("Belbosaur", 22, 100, "Humans");
+var cat = new VirtualPet("Mia", 25, 100, "Mice");
+            
+dragon.Feed("Humans");
+dragon.Feed("Apple");
+dragon.Feed();
+cat.Feed("Orange");
+cat.Feed("Mice");
---
+++ Ukesoppgave Uke3/VirtualPet.cs
@@ -0,0 +1,35 @@
+﻿namespace Ukesoppgave_Uke3;
+
+public class VirtualPet
+{
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+    public int Hunger { get; private set; }
+    public string FavoriteFood { get; }
+
+    public VirtualPet(string name, string favoriteFood)
+    {
+        Name = name;
+        FavoriteFood = favoriteFood;
+    }
+
+    public VirtualPet(string name, int age, int hunger, string favoriteFood)
+    {
+        Name = name;
+        Age = age;
+        Hunger = hunger;
+        FavoriteFood = favoriteFood;
+    }
+
+    public void Feed()
+    {
+        Console.WriteLine("Takk for maten!");
+    }
+
+    public void Feed(string incomingFood)
+    {
+        Console.WriteLine(incomingFood == FavoriteFood
+            ? "WOW! Favoritt Maten min!"
+            : "Ikke favoritten min, men det funker! Takk<3");
+    }
+}
